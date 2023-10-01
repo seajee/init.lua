baaa2284b0abb754e4a257a4833e553183714b82
seajee <teapods@proton.me>
2023-10-01 19:46:56 +0200

    Added keymap for man pages

--- lua/seajee/remap.lua
+++ lua/seajee/remap.lua
@@ -24,3 +24,5 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>m", "<cmd>:Man<CR>")
