b8f19957482dacd1372d349c8bcbd9b52eb22415
seajee <teapods@proton.me>
2023-10-31 16:28:35 +0100

    Remove reduntant vim commmand for packer

--- lua/seajee/packer.lua
+++ lua/seajee/packer.lua
@@ -1,7 +1,3 @@
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
 return require("packer").startup(function(use)
     -- Packer can manage itself
     use "wbthomason/packer.nvim"
