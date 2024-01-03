c8484f99f625fbcea79cdf7777a0e902c1875f72
seajee <teapods@proton.me>
2024-01-03 19:33:25 +0100

    Changed colorscheme configurations

--- lua/seajee/colors.lua
+++ lua/seajee/colors.lua
@@ -1,4 +1,27 @@
+-- Configure gruvbox colorscheme
+require("gruvbox").setup({
+    italic = {
+        strings = false,
+        emphasis = false,
+        comments = false,
+        operators = false,
+        folds = false,
+    },
+    -- TODO
+    palette_overrides = {},
+})
+
 vim.cmd.colorscheme("gruvbox")
+
+-- Highlight trailing whitespaces only in NORMAL mode
+vim.cmd([[
+augroup HighlightWhitespace
+  autocmd!
+  autocmd CursorMoved * match errorMsg /\s\+$/
+  autocmd InsertEnter * match none
+  autocmd InsertLeave * match errorMsg /\s\+$/
+augroup END
+]])
 
 -- Do not use vim background
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
