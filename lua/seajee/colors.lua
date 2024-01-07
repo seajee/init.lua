cd8af949ef8e48eab90b08b5e96cefb64825f434
seajee <teapods@proton.me>
2024-01-07 18:44:33 +0100

    Disabled trailing whitespace display

--- lua/seajee/colors.lua
+++ lua/seajee/colors.lua
@@ -14,14 +14,14 @@ require("gruvbox").setup({
 vim.cmd.colorscheme("gruvbox")
 
 -- Highlight trailing whitespaces only in NORMAL mode
-vim.cmd([[
-augroup HighlightWhitespace
-  autocmd!
-  autocmd CursorMoved * match errorMsg /\s\+$/
-  autocmd InsertEnter * match none
-  autocmd InsertLeave * match errorMsg /\s\+$/
-augroup END
-]])
+--vim.cmd([[
+--augroup HighlightWhitespace
+--  autocmd!
+--  autocmd CursorMoved * match errorMsg /\s\+$/
+--  autocmd InsertEnter * match none
+--  autocmd InsertLeave * match errorMsg /\s\+$/
+--augroup END
+--]])
 
 -- Do not use vim background
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
