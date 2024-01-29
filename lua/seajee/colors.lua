2e704074532794efbe212a4abf13a2240038873e
seajee <teapods@proton.me>
2024-01-29 21:31:31 +0100

    Highlighting of whitespaces, tabs etc.

--- lua/seajee/colors.lua
+++ lua/seajee/colors.lua
@@ -13,16 +13,21 @@ require("gruvbox").setup({
 
 vim.cmd.colorscheme("gruvbox")
 
--- Highlight trailing whitespaces only in NORMAL mode
---vim.cmd([[
---augroup HighlightWhitespace
---  autocmd!
---  autocmd CursorMoved * match errorMsg /\s\+$/
---  autocmd InsertEnter * match none
---  autocmd InsertLeave * match errorMsg /\s\+$/
---augroup END
---]])
-
 -- Do not use vim background
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+-- Highlight whitespaces and traling whitespace
+vim.cmd.set("list")
+vim.cmd.set("listchars+=space:·")
+vim.cmd.set("listchars+=trail:·")
+
+-- Highlight tabs, eol (end-of-line), and non-breaking spaces
+vim.cmd.set("listchars+=tab:\\→\\ ")
+--vim.cmd.set("listchars+=eol:↴")
+vim.cmd.set("listchars+=nbsp:_")
+
+-- Highlight special characters, such as invisible characters (non-printable)
+vim.cmd.set("listchars+=extends:>")
+vim.cmd.set("listchars+=precedes:<")
+vim.cmd.set("listchars+=nbsp:_")
