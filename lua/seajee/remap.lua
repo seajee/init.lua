85f0ff715a41b03763a15bc317cedf42894ea7d8
seajee <teapods@proton.me>
2024-01-30 18:53:10 +0100

    New keybind for grepping

--- lua/seajee/remap.lua
+++ lua/seajee/remap.lua
@@ -33,6 +33,9 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 -- Replace all
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- Vim Grep
+vim.keymap.set("n", "<leader>vs", [[:vimgrep // **/*<Left><Left><Left><Left><Left><Left>]])
+
 -- Make executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
